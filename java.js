f129064159a9e43f2764660e986d2d4ab1d8ec33
ankitabasera <65150873+ankitabasera@users.noreply.github.com>
2020-07-16 10:12:37 +0530

    Create java.js

---
+++ java.js
@@ -0,0 +1,30 @@
+function changetheme()
+{
+	var v1 = document.body; 
+	var theme = v1.classList;
+	if(theme == "light")
+	{
+		theme.toggle("dark");
+		document.getElementById("para").innerHTML = "Current theme is Dark.";
+
+	}
+	else
+	{
+		theme.toggle("light");
+		document.getElementById("para").innerHTML = "Current theme is Light.";
+	}
+	//contentchange();
+}
+/*function changetheme()
+{
+	content = document.getElementById("para");
+	if(content.innerHTML === "Current theme is Dark.");
+	{
+		content.innerHTML = "Current theme is Light.";
+
+	}
+	else
+	{
+		content.innerHTML = "Current theme is Dark.";
+	}
+}*/
